1460cbc5d9c5dfe4013eabdc6eea6c9a758f9881
Eryos <capa5021@gmail.com>
2026-05-29 14:45:33 +0200

    disabled most incompatible options

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,6 +9,14 @@ export class Main {
   static StartingTime: number = 0
 }
 
+app.disableHardwareAcceleration();
+app.commandLine.appendSwitch('no-sandbox');
+app.commandLine.appendSwitch('disable-gpu');
+app.commandLine.appendSwitch('disable-software-rasterizer');
+app.commandLine.appendSwitch('disable-gpu-compositing');
+app.commandLine.appendSwitch('disable-gpu-rasterization');
+app.commandLine.appendSwitch('disable-gpu-sandbox');
+
 app.whenReady().then(() => {
   new ViewController();
   let networkListener = new NetworkListerner();
